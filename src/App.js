84d3f893848b7ebc1978083fe226de68f0ad2eb1
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-01-25 15:19:38 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
 import "./App.css";
+import { useTelegram } from "./components/hooks/useTelegram";
 
 function App() {
+  const { onToggleButton, tg } = useTelegram();
   useEffect(() => {
     tg.ready();
   }, []);
 
   return (
     <div className="App">
-      working
-      <button onClick={onClose}>Закрыть</button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
